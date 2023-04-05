106616d13293c568c8c299f12bc1b9effadd6d0d
Marcos Lima <marcos.lima@icomp.ufam.edu.br>
2023-04-05 19:57:40 -0400

    feat(main): add project main file'

---
+++ scripts/main.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# UNIVERSIDADE FEDERAL DO AMAZONAS
+# INSTITUTO DE COMPUTAÇÃO
+# BANCO DE DADOS I - 2022/02 (2023)
+# PROFESSOR: ALTIGRAN SOARES DA SILVA
+# ALUNOS: JOSÉ MATEUS CORDOVA RODRIGUES
+# ALUNOS: GIOVANNA ANDRADE DOS SANTOS
+# ALUNOS: MARCOS AVNER PIMENTA DE LIMA
+# TRABALHO PRÁTICO I
+
+from database import DatabaseManager
+from datetime import datetime
+
+if __name__ == '__main__':
+    start_time = datetime.now()
+    DatabaseManager.create_database(DatabaseManager.POSTGRESQL_DB)
+    # dataset_path = input('INFORME O LOCAL DO ARQUIVO DE METADADOS: ')
+    # products, categories, prod_cats, similars, reviews = AmazonDatasetController().extrair(dataset_path)
+
+    # ProductController.insert_batch(products)
+    # print(f'{len(products)} PRODUCTS INSERTED INTO DATABASE')
+    # CategoryController.insert_batch(categories.values())
+    # print(f'{len(categories.values())} CATEGORIES INSERTED INTO DATABASE')
+    # ProductCategoryController.insert_batch(prod_cats)
+    # print(f'{len(prod_cats)} PRODUCT CATEGORIES INSERTED INTO DATABASE')
+    # SimilarProductsController.insert_batch(similars)
+    # print(f'{len(similars)} SIMILARS PRODUCTS INSERTED INTO DATABASE')
+    # ReviewController.insert_batch(reviews)
+    # print(f'{len(reviews)} PRODUCT REVIEWS INSERTED INTO DATABASE')
+    DatabaseManager.close_connection()
+    print(f'TIME ELAPSED: {datetime.now() - start_time}')
